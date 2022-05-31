c444c481c07fc376fc3ea56f193c3de1d47dc100
Haidar <68181532+hilmyha@users.noreply.github.com>
2022-06-01 04:17:33 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res) {
     response.ok("aplikasi REST API sudah berjalan", res)
 };
+
+// show all data mahasiswa
+exports.tampilsemua = function(req, res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
