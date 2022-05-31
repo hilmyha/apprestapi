4609c21fd6ab8fd0e4fa704f89384ef8b2c8b451
Haidar <68181532+hilmyha@users.noreply.github.com>
2022-06-01 04:07:21 +0700

    setting controller js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-    response.ok("aplikasi REST API sudah berjalan")
+    response.ok("aplikasi REST API sudah berjalan", res)
 };
