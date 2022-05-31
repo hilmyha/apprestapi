0ff652fe4b7645e09361a24b57cf5667c84aca03
Haidar <68181532+hilmyha@users.noreply.github.com>
2022-06-01 05:49:36 +0700

    berhasil menambahkan edit dan hapus data

--- controller.js
+++ controller.js
@@ -46,3 +46,32 @@ exports.tambahmahasiswa = function(req, res) {
             }
         });
 };
+
+// mengubah data berdasarkan id
+exports.editmahasiswaid = function(req, res) {
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?', [nim, nama, jurusan, id],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data berhasil diubah", res);
+            }
+        });
+};
+
+// menghapus data berdasarkan id
+exports.hapusmahasiswaid = function(req, res) {
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data berhasil dihapus", res);
+            }
+        });
+}
