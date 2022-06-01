61af9074d170146eda4b160291395bc26ee19a1d
Haidar <68181532+hilmyha@users.noreply.github.com>
2022-06-01 11:47:57 +0700

    nested json objek berhasil dibuat

--- routers.js
+++ routers.js
@@ -20,4 +20,7 @@ module.exports = function(app) {
 
     app.route('/hapus')
         .delete(jsonku.hapusmahasiswaid);
+
+    app.route('/tampilmatakuliah')
+        .get(jsonku.tampilgroupmatkul);
 }
